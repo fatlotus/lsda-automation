ec5a495ad5e966705b5bcf2831ed013b901050bf
Jeremy Archer <open-source@fatlotus.com>
2014-04-23 13:35:19 -0500

    Add scale-up automaton.

---
+++ scale_up.py
@@ -0,0 +1,63 @@
+import pika
+from boto.ec2.autoscale import AutoScaleConnection
+import time
+import logging
+import yaml
+
+def get_queue_length(channel, queue_name):
+    """
+    Measures the length of the given queue.
+    """
+
+    return channel.queue_declare(
+        queue = queue_name,
+        durable = True,
+        exclusive = False,
+        auto_delete = False,
+        passive = True
+    ).method.message_count
+
+def main():
+    """
+    Main entry point for the automated scaling daemon.
+    """
+
+    # Read configuration.
+    options = yaml.load(open("config.yaml"))
+
+    # Connect to the RabbitMQ cluster.
+    params = pika.ConnectionParameters(host=options["amqp"])
+    conn = pika.BlockingConnection(params)
+
+    channel = conn.channel()
+
+    # Fetch the ASG.
+    group = AutoScaleConnection().get_all_groups(["LSDA Worker Pool"])[0]
+
+    while True:
+        # See if we have 5s of activity.
+        for i in xrange(5):
+            queue_length = get_queue_length(channel, "stable")
+
+            print("Queue length: {}".format(queue_length))
+
+            if queue_length == 0:
+                break
+            time.sleep(1)
+
+        else:
+            # Scale up!
+
+            group.desired_capacity = min(
+              group.desired_capacity + 2, group.max_size)
+            group.update()
+
+            print("Triggering increase to {}".format(group.desired_capacity))
+
+            time.sleep(120)
+
+        # Wait until next polling event.
+        time.sleep(30)
+
+if __name__ == '__main__':
+    main()
